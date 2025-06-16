0b8e10b79a06ad8710371a3598342f875494d0ae
Humayra Chowdhury <hc1415926@gmail.com>
2025-06-17 00:49:27 +0100

    Created movement functionality of paddle based on user input

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
   This file contains the code that draws the game elements onto the canvas.
 
   Author: Humayra Chowdhury
-  Version: 1.0
+  Version: 1.1
   File: script.js
 */
 
@@ -15,8 +15,32 @@ const canvas = document.getElementById('game-canvas');
 // Allows canvas to be used in drawing 2D graphics
 const ctx = canvas.getContext('2d');
 
+// Initial paddle information
+let paddleY = 250;
+const paddleHeight = 100;
+const paddleSpeed = 5;
+
+// Track key presses
+const keys = {};
+
+document.addEventListener ('keydown', (e) => {
+  keys[e.key.toLowerCase()] = true;
+});
+
+document.addEventListener ('keyup', (e) => {
+  keys[e.key.toLowerCase()] = false;
+});
+
 // Draw game
 function draw() {
+
+  // Update paddle position based on key input
+  if ((keys['w'] || keys['arrowup']) && paddleY > 0) {
+    paddleY -= paddleSpeed; // Move up
+  }
+  if ((keys['s'] || keys['arrowdown']) && paddleY < canvas.height - paddleHeight) {
+    paddleY += paddleSpeed; // Move down
+  }
 
   // Clear canvas
   ctx.fillStyle = 'black';
@@ -24,7 +48,7 @@ function draw() {
 
   // Draw paddle
   ctx.fillStyle = "white";
-  ctx.fillRect(20, 250, 10, 100); // x, y, width, height
+  ctx.fillRect(20, paddleY, 10, paddleHeight); // x, y, width, height
 }
 
 // Call draw() repeatedly 
