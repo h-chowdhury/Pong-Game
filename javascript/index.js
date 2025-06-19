0034a8f96c936b26c1d17d08b6d72fcb0929c26c
Humayra Chowdhury <hc1415926@gmail.com>
2025-06-19 23:35:45 +0100

    Added code which updates the 'best score' value on the home page

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,18 @@
 /* ========================================
-    'Star Game' button code
+    'Best score' code
+   ======================================== */
+
+if (localStorage.getItem('bestScore') === null) {
+  localStorage.setItem('bestScore', 0);
+}
+
+let best_score_text = document.getElementById("best-score-text");
+best_score_text.innerText = "Best score: " + localStorage.getItem('bestScore');
+
+
+
+/* ========================================
+    'Start Game' button code
    ======================================== */
 
 function start_game () {
